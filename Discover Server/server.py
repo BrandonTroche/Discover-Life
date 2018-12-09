ba7e2683189ac3aaf2cf64d4b9c87789fc7d3d5c
Brandon T <bttroche@gmail.com>
2018-12-08 21:19:14 -0800

    pod install mapbox + Alamofire
    Server.py changes:
      - ATM API
      - Currency API
      - Tip API
      - Offers API

--- Discover Server/server.py
+++ Discover Server/server.py
@@ -37,16 +37,85 @@ def location():
 def check_events():
    pass
 
-@app.route('/test', methods=['GET'])
-def test():
-    # url = 'https://api.discover.com/cityguides/v2/cities'
+@app.route('/cityguides/merchants', methods=['GET'])
+def cityguides_merchants():
     headers = {  'Accept':'application/json',  'x-dfs-api-plan':'CITYGUIDES_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
-    # request = Request(url, headers=headers)
-    # request.get_method = lambda: 'GET'
-    # response_body = urlopen(request).read()
-    response_body = requests.get('https://api.discover.com/cityguides/v2/cities', headers = headers)
+    response_body = requests.get('https://api.discover.com/cityguides/v2/merchants', headers = headers)
     return(json.dumps(response_body.text), 200)
+
+@app.route('/cityguides/healthcheck', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'CITYGUIDES_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/cityguides/v2/healthcheck', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/cityguides/categories', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'CITYGUIDES_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/cityguides/v2/categories', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/atm/healthcheck', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_ATM_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/atm/v1/healthcheck', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/atm/locations', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_ATM_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/atm/v1/locations', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/tip/healthcheck', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_TIPETIQUETTE_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/tip/v1/healthcheck', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/tip/guide', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_TIPETIQUETTE_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/tip/v1/guide', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/currency/healthcheck', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_CURRENCYCONVERSION_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/currencyconversion/v1/healthcheck', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/currency/exchangerate', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_CURRENCYCONVERSION_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/currencyconversion/v1/exchangerate', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/currency/languages', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCI_CURRENCYCONVERSION_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci/currencyconversion/v1/languages', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/offers/healthcheck', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCIOFFERS_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci-offers/v2/healthcheck', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+@app.route('/offers/offers', methods=['GET'])
+def cityguides_merchants():
+    headers = {  'Accept':'application/json',  'x-dfs-api-plan':'DCIOFFERS_SANDBOX',  'Authorization':'Bearer c2cbee55-61a6-45ec-bdfd-d26e9ade89ed'  }
+    response_body = requests.get('https://api.discover.com/dci-offers/v2/offers', headers = headers)
+    return(json.dumps(response_body.text), 200)
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=8080, use_reloader = False, threaded = True)
+
+
+
+
